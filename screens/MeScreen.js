6b60d8585996486d22305e04ae6e7b15ddb003cc
Peaw1998 <m.peaw5500@gmail.com>
2019-04-19 16:00:44 +0700

    Me Screen

--- screens/MeScreen.js
+++ screens/MeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { View, Text, StyleSheet, H2 } from 'react-native'
-import { Container, Header, Content, Card, CardItem, Thumbnail, Button, Icon, Left, Body, Right } from 'native-base'
+import { View, StyleSheet, Image, H2 } from 'react-native'
+import { Container, Header, Content, List, ListItem, Left, Body, Right, H1, Thumbnail, Text } from 'native-base'
 
 export default class MeScreen extends React.Component {
 	static navigationOptions = {
@@ -9,11 +9,89 @@ export default class MeScreen extends React.Component {
 
 	render() {
 		return (
-			<View style={styles.container}>
-				<Container>
-				<Text>eiei</Text>
-				</Container>
-			</View>
+			<Container>
+
+				<Content>
+					<Text>  </Text>
+					<View style={font.Viewcenter}>
+						<Image source={require('../pics/User.png')} style={font.size}></Image>
+						<H1>User name</H1>
+					</View>
+					<Text>  </Text>
+
+
+					<List>
+						<ListItem avatar>
+							<Left>
+								<Text>Lesson 1</Text>
+							</Left>
+							<Body>
+								<Text>  </Text>
+								<Text>  </Text>
+							</Body>
+							<Right>
+								<Text note>100 %</Text>
+							</Right>
+						</ListItem>
+					</List>
+					<List>
+						<ListItem avatar>
+							<Left>
+								<Text>Lesson 2</Text>
+							</Left>
+							<Body>
+								<Text>  </Text>
+								<Text>  </Text>
+							</Body>
+							<Right>
+								<Text note>50 %</Text>
+							</Right>
+						</ListItem>
+					</List>
+					<List>
+						<ListItem avatar>
+							<Left>
+								<Text>Lesson 3</Text>
+							</Left>
+							<Body>
+								<Text>  </Text>
+								<Text>  </Text>
+							</Body>
+							<Right>
+								<Text note>10 %</Text>
+							</Right>
+						</ListItem>
+					</List>
+					<List>
+						<ListItem avatar>
+							<Left>
+								<Text>Lesson 4</Text>
+							</Left>
+							<Body>
+								<Text>  </Text>
+								<Text>  </Text>
+							</Body>
+							<Right>
+								<Text note>0 %</Text>
+							</Right>
+						</ListItem>
+					</List>
+					<List>
+						<ListItem avatar>
+							<Left>
+								<Text>Lesson 5</Text>
+							</Left>
+							<Body>
+								<Text>  </Text>
+								<Text>  </Text>
+							</Body>
+							<Right>
+								<Text note>0 %</Text>
+							</Right>
+						</ListItem>
+					</List>
+				</Content>
+			</Container>
 		)
 	}
 }
@@ -23,4 +101,16 @@ const styles = StyleSheet.create({
 		flex: 1,
 		backgroundColor: '#fff'
 	}
+})
+const font = StyleSheet.create({
+	Viewcenter: {
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	size: {
+		width: 100,
+		height: 100,
+	}
+
+
 })
